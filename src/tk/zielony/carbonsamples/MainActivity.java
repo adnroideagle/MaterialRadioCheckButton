f667a0639b6a12ea44a929f19fa0e5be8c181257
mi.rufeng <958936985@qq.com>
2016-01-07 09:28:25 +0800

    first commit

---
+++ src/tk/zielony/carbonsamples/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.zielony.carbonsamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import carbon.widget.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
